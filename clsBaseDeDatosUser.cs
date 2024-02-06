0a2dca2f63de1611f2b483e066a42c1672e944b6
nahuelsosa2105 <nahuelsosa2105@gmail.com>
2024-02-05 22:13:33 -0300

    Creacion de clase para la base de datos

---
+++ clsBaseDeDatosUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace pryBancoDeSangre
+{
+    internal class clsBaseDeDatosUser
+    {
+        OleDbConnection conexionBd;
+        OleDbCommand comandoBd;
+        OleDbDataReader lectorBd;
+
+
+
+
+    }
+}
